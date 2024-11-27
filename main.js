2834e7198d5cb6ee61b5d0172c3dad92e2fb9aab
Hamidullo05 <hamidulloegamberdiyev067@gmail.com>
2024-11-27 07:28:16 +0500

    work

---
+++ main.js
@@ -0,0 +1,314 @@
+// 1-masala
+let num1_1 = +prompt("1-masala")
+let num1_2 = +prompt("1-masala")
+if (num1_1>0) {
+    console.log("1-son musbat")
+
+}
+if (num1_2>0) {
+    console.log("2-son musbat")
+    
+} else {
+    console.log("ikkala son ham manfiy")
+}
+// 2-masala
+let num2_1 = +prompt("2-masala")
+let num2_2 = +prompt("2-masala")
+let num2_3 = +prompt("2-masala")
+if (num2_1 <0 || num2_2<0 || num2_3<0) {
+    console.log("kiritga qiymatlariz ichida manfiy son bor")
+} else {
+    console.log("kiritilgan sonlarni hammasi musbat")
+}
+// 3-masala
+let num3 = +prompt("3-masala")
+if (num3%2==0 && num3>0) {
+    console.log("kiritilgan sonlar musbat va juft")
+} else {
+    console.log("kiritilgan sonlar shartlani qanotlantirmadi ")
+}
+// 4-masala
+let num4_1 = +prompt("4-masala")
+let num4_2 = +prompt("4-masala")
+if ((num4_1 + num4_2) > (num4_1 * num4_2)) {
+    console.log("Katta")
+} else {
+    console.log("Kichik")
+}
+// 5-masala
+let num5 = +prompt("5-masala")
+if (100<num5<999) {
+    console.log("kiritilgan son 100 dan katta 999 dan esa kichik")
+} else {
+    console.log("kiritilgan son berilgan oraligda emas")
+}
+// 6-dars
+let num6_1 = +prompt("6-masala")
+let num6_2 = +prompt("6-masala")
+let num6_3 = +prompt("6-masala")
+if ((num6_1 + num6_2 + num6_3) >0) {
+    console.log("musbat")
+} else {
+    console.log("manfiy")
+} 
+// 7-dars
+let num7_1 = +prompt("7-masala")
+let num7_2 = +prompt("7-masala")
+if (num7_1 !=0 && num7_2 !=0) {
+    console.log("0 ga teng emas")
+}
+// 8-masala
+let num8_1 = +prompt("8-masala")
+let num8_2 = +prompt("8-masala")
+if ((num8_1 * num8_2)>0 && (num8_1 - num8_2) <0 ) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 9-dars
+let num9 = +prompt("9-masala")
+if (99<num9<1000 && num9>0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 10-dars
+let num10 = +prompt("10-masala")
+if (num10%10==0 ) {
+    console.log("true")
+} else {
+    console.log("folse")
+
+}
+// 11-masala
+let num11_1 = +prompt("11-masala")
+let num11_2 = +prompt("11-masala")
+let num11_3 = +prompt("11-masala")
+if (num11_1==num11_2 || num11_2 == num11_3 || num11_1 == num11_3) {
+    console.log("true")
+
+} else {
+    console.log("folse")
+}
+// 12-masala
+let num12 = +prompt("12-masala")
+if (num12%7==0 && num12%3!=0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 13-masala
+let num13_1 = +prompt("13-masala")
+let num13_2 = +prompt("13-masala")
+if ((num13_1==0 ||num13_2==0) || (num13_1<0 && num13_2<0)) {
+    console.log("true")
+} else {
+  console.log("folse")
+}
+// 14-masala
+let num14_1 = +prompt("14-masala")
+let num14_2 = +prompt("14-masala")
+let num14_3 = +prompt("14-masala")
+if ((num14_1<num14_3 && num14_2<num14_3) && num14_3>0) {
+    console.log("true")
+}
+if ((num14_1<num14_2 && num14_2>num14_3) && num14_2>0) {
+    console.log("true")
+}
+if ((num14_1>num14_3 && num14_2<num14_1) && num14_1>0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 15-masala
+let num15 = +prompt("15-masala")
+if (num15<=10 || num15>=20) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 16-masala
+let num16_1 = +prompt("16-masala")
+let num16_2 = +prompt("16-masala")
+if ((num16_1 + num16_2)<0 || (num16_1 * num16_2)>0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 17-masala
+let num17_1 = +prompt("17-masala")
+let num17_2 = +prompt("17-masala")
+let num17_3 = +prompt("17-masala")
+if ((num17_1>num17_3 && num17_2>num17_3) && num17_3%2==0) {
+    console.log("true")
+}
+if ((num17_1>num17_2 && num17_2<num17_3) && num17_2%2==0) {
+    console.log("true")
+}
+if ((num17_1<num17_3 && num17_2>num17_1) && num14_1%2==0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 18-masala
+let num18 = +prompt("18-masala")
+if (num18%10==5 || num18%10==0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 19-masala
+let num19_1 = +prompt("19-masala")
+let num19_2 = +prompt("19-masala")
+if (num19_1>0 && num19_2<0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 20-masala
+let num20_1 = +prompt("20-masala")
+let num20_2 = +prompt("20-masala")
+let num20_3 = +prompt("20-masala")
+if (num20_1%2==0 || num20_2%2==0 || num20_3%2==0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 21-masala
+ /*let num21 = +prompt("21-masala")
+if (num21%10==a && num21%100 ) */
+// 22-masala
+let num22_1 = +prompt("22-masala")
+let num22_2 = +prompt("22-masala")
+if (num22_1%num22_2==0 || num22_2%num22_1==0) {
+    console.log("karrali")
+} else {
+    console.log("karrali emas")
+}
+// 23-masala
+let num23_1 = +prompt("23-masala")
+let num23_2 = +prompt("23-masala")
+let num23_3 = +prompt("23-masala")
+if ((num23_1 + num23_2 + num23_3)==0 || (num23_1 * num23_2 * num23_3)<0 ) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 24-masala
+ /*let num24 = +prompt("24-masala")
+if () */
+// 25-masala
+let num25_1 = +prompt("25-masala")
+let num25_2 = +prompt("25-masala")
+if ((num25_1%2==1 || num25_2%==1) && (num25_1%2==0 || num25_2%==0)) { 
+    console.log("true")
+} else {
+    console.log("folse")
+} 
+// 26-masala
+let num26 = +prompt("26-masala")
+if (num26>0 && (num16<100 && num26>999)) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 27-masala
+let num27_1 = +prompt("27-masala")
+let num27_2 = +prompt("27-masala")
+let num27_3 = +prompt("27-masala")
+if ((num27_1 + num27_2 + num27_3)>100 || (num27_1 * num27_2 * num27_3)<0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 28-masala
+let num28_1 = +prompt("28-masala")
+let num28_2 = +prompt("28-masala")
+if ((num28_1>10 || num28_2>10) && (num28_1<0 || num28_2<0)) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 29-masala
+let num29 = +prompt("29-masala")
+if (num29%2==1 && num29>0) {
+    console.log("Ha")
+
+} else {
+    console.log("Yoq")
+}
+// 30-masala
+let num30_1 = +prompt("30-masala")
+let num30_2 = +prompt("30-masala")
+if (num30_1 >0 && num30>0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 31-masala
+let num31 = +prompt("31-masala")
+if (num31%2==0 && num31&4==0 && num31%8!=0) {
+    console.log("true")
+} else  {
+    console.log("folse")
+}
+// 32-masala
+let num32_1 = +prompt("32-masala")
+let num32_2 = +prompt("32-masala")
+let num32_3 = +prompt("32-masala")
+if (num32_1 <0 || num32_2<0 || num32_3<0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 33-masala
+/*let num32 = +prompt("32-masala")
+if (num33%10==0)*/
+// 34-masala
+/*let num34 = +prompt("34-masala")
+let num34 = +prompt("34-masala")
+if ()*/
+// 35-masala
+let num35 = +prompt("35-masala")
+if (num35>0 && num<1000) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 36-masala
+let num36_1 = +prompt("36-masala")
+let num36_2 = +prompt("36-masala")
+if ((num36_1%2==0 || num36_2%2==0) && (num36_1 * num36_2)>20) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 37-masala
+let num37_1 = +prompt("37-masala")
+let num37_2 = +prompt("37-masala")
+let num37_3 = +prompt("37-masala")
+if (num37_1%10==0 || num37_2%10==0 || num37_3==0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+//38-masala
+let num38 = +prompt("38-masala")
+if (num38%4==0  && num38%2==0) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 39-masala
+let num39_1 = +prompt("39-masala")
+let num39_2 = +prompt("39-masala")
+let num39_3 = +prompt("39-masala")  
+if ((num39_1+num39_2)>num39_3 || (num39_1+num39_3)>num39_2 || (num39_3+num39_2)>num39_1) {
+    console.log("true")
+} else {
+    console.log("folse")
+}
+// 40-masala
+let num40_1 = +prompt("40-masala")
+let num40_2 = +prompt("40-masala")
+if ()
